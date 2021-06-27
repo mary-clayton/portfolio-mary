577a30b7c0586c814fee0868f0553e39a73a3004
mary-clayton <mary.cathlean@gmail.com>
2021-06-27 16:23:37 -0700

    finished main page

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,9 +1,9 @@
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 import styled from 'styled-components';
 
 const Alink = styled.a `
-padding: 30%;
+padding: 0 30%;
 `
 const Navbar = () => {
     return ( 
@@ -12,11 +12,21 @@ const Navbar = () => {
                 <h1>Mary Clayton</h1>
             </div>
         <nav className= {styles.navbar}>
-            <Link href="/"><Alink>Home</Alink></Link>
-            <Link href="/about"><Alink>About</Alink></Link>
-            <Link href="/projects"><Alink>Projects</Alink></Link>
-            <Link href="/contact"><Alink>Contact</Alink></Link>
-            <Link href="/blog"><Alink>Blog</Alink></Link>
+            <Link href="/">
+                <Alink>Home</Alink>
+                </Link>
+            <Link href="/about">
+                <Alink>About</Alink>
+                </Link>
+            <Link href="/projects">
+                <Alink>Projects</Alink>
+                </Link>
+            <Link href="/contact">
+                <Alink>Contact</Alink>
+                </Link>
+            <Link href="/blog">
+                <Alink>Blog</Alink>
+                </Link>
         </nav>
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
 
@@ -6,7 +7,16 @@ export default function Home() {
   return (
     <div className={styles.container}>
      <Navbar />
+     <div className={styles.bg}>
+     <div className={styles.into}>
+       <div className={styles.words}>
+     <h2>Hi, My name is Mary Clayton.</h2>
+     <p>An AWS Certified Software Engineer who is proficient in Full-stack Web Applications.</p>
+     </div>
+     <img className= {styles.profile} src="/me.jpg"/>
+     </div>
      <Footer />
+     </div>
     </div>
   )
 }
