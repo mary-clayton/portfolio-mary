cd701e1789b878e5bd939ff765c775f96dd166b0
mary-clayton <mary.cathlean@gmail.com>
2021-06-29 17:37:34 -0700

    added more details to projects

--- pages/about.js
+++ pages/about.js
@@ -5,12 +5,16 @@ const About = () => {
     return ( 
         <div>
              <Navbar/>
-        <div className={styles.about}>
+        <div className={styles.maindiv}>
             <section className={styles.head}>
             <h1>About</h1>
-            <p className={styles.para1}>I am a software developer located in Phoenix, AZ, currently seeking a career back in Software Development and cloud development.
+            <p className={styles.para1}>I am a software developer and designer located in Phoenix, AZ, currently seeking a career in Software Development, design, and cloud development.
                 I am AWS-certified as a Cloud Practioner and a systems Ops Associate. I currently am working towards a Bachelors in Computer Science
-                in University of the People and a certification in System Design and Solutions at DeVry University.
+                in University of the People and a certification in System Design and Solutions at DeVry University. I currently hold a Bachelors of Art
+                in Digital Design from Grand Canyon University.
+            </p>
+            <p className={styles.para1}> I am a technical and design-oriented person who enjoys creating and helping others achieve a common
+            goal. I am a fast learner who is able to pick up on skills and pick up multiple tasks at once with ease.
             </p>
             <button> <a href="https://drive.google.com/file/d/10ZJJVH3okRTzUkAN1Qak1fmey8soAe0T/view?usp=sharing">Resume</a></button>
             </section>
--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,38 @@
 import Navbar from '../comps/Navbar'
+import styles from '../styles/Home.module.css'
 const Projects = () => {
     return ( 
         <div>
             <Navbar/>
+            <div className={styles.maindiv}>
+            <section className={styles.head}>
+            <h1>Projects</h1>
+            <div className= {styles.card}>
+            <h3>Food Truck Trackr</h3>
+            <img className= {styles.cardimage} src="./tablet_foodtruck.png"/>
+            <button className={styles.codebutton}><a href="https://github.com/BW-WEBPT15-FoodTruckTrackR4">View Code</a></button>
+            </div>
+            </section>
+            
+            
+            <section className={styles.head}>
+            <div className= {styles.card}>
+            <h3>Restaurant Passport</h3>
+            <img className= {styles.cardimage} src=""/>
+            <button className={styles.codebutton}><a href="">View Code</a></button>
+            </div>
+            </section>
+           
+
+           
+            <section className={styles.head}>
+            <div className= {styles.card}>
+            <h3>KidsFly</h3>
+            <img className= {styles.cardimage} src=""/>
+            <button className={styles.codebutton}><a href="">View Code</a></button>
+            </div>
+            </section>
+           </div>
         </div>
      );
 }
